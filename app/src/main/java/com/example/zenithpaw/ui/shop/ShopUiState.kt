bdbfb42b74bb5ac0f0f0495b390293364301c731
Tony Faijue <tonyf17191@gmail.com>
2026-05-14 18:28:16 -0500

    Added new UiStates & ViewModel files, Modified some UiStates

--- app/src/main/java/com/example/zenithpaw/ui/shop/ShopUiState.kt
+++ app/src/main/java/com/example/zenithpaw/ui/shop/ShopUiState.kt
@@ -1,6 +1,19 @@
 package com.example.zenithpaw.ui.shop
 
+import com.example.zenithpaw.roomdatabase.shopitem.ShopItem
+import com.example.zenithpaw.ui.shopitem.ShopItemUiState
+
 data class ShopUiState(
+    //Shop identifiers
     val shopId: String = "",
-    val name: String = "",
+    val name: String = "Loading Shop...",
+
+    //Data
+    val availableItems: List<ShopItemUiState> = emptyList(),
+    val userBalance: Int = 0,
+
+    //UI Logic State
+    val selectedItem: ShopItem? = null, // For showing preview dialog
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null
 )
--- app/src/main/java/com/example/zenithpaw/ui/userinventoryitem/UserInventoryItemUiState.kt
+++ app/src/main/java/com/example/zenithpaw/ui/userinventoryitem/UserInventoryItemUiState.kt
@@ -2,5 +2,7 @@ package com.example.zenithpaw.ui.userinventoryitem
 
 data class UserInventoryItemUiState(
     val inventoryItemId: String = "",
+    val name: String = "",
+    val imageUrl: String = "",
     val quantity: Int = 0
 )

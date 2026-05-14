e3352becbbe13a34322eb337edc98270c8716647
Tony Faijue <tonyf17191@gmail.com>
2026-05-13 20:03:53 -0500

    Added UiStates for Data Objects

---
+++ app/src/main/java/com/example/zenithpaw/roomdatabase/shopitem/ShopItemUiState.kt
@@ -0,0 +1,9 @@
+package com.example.zenithpaw.roomdatabase.shopitem
+
+data class ShopItemUiState(
+    val shopItemId: String = "",
+    val name: String = "",
+    val imageUrl: String = "",
+    val price: Int = 0,
+    val description: String = "",
+)
---
+++ app/src/main/java/com/example/zenithpaw/roomdatabase/userinventoryitem/UserInventoryItemUiState.kt
@@ -0,0 +1,6 @@
+package com.example.zenithpaw.roomdatabase.userinventoryitem
+
+data class UserInventoryItemUiState(
+    val inventoryItemId: String = "",
+    val quantity: Int = 0
+)

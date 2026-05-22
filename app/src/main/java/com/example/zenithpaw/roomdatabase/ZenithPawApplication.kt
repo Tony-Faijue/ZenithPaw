ce7a4e5718df174b8a53759889c5b294e5419a1f
Tony Faijue <tonyf17191@gmail.com>
2026-05-21 22:50:47 -0500

    Added AppLifecycleManager to handle lifecycle events of the app along with dependencies

--- app/src/main/java/com/example/zenithpaw/roomdatabase/ZenithPawApplication.kt
+++ app/src/main/java/com/example/zenithpaw/roomdatabase/ZenithPawApplication.kt
@@ -1,8 +1,49 @@
 package com.example.zenithpaw.roomdatabase
 
 import android.app.Application
+import android.util.Log
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ProcessLifecycleOwner
+import com.example.zenithpaw.AppLifecycleManager
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class ZenithPawApplication: Application() {
+class ZenithPawApplication: Application(), DefaultLifecycleObserver {
+    private val tag = "ZenithPawApplication Lifecycle"
+    @Inject
+    lateinit var appLifecycleManager: AppLifecycleManager
+
+    // Lifecycle observer methods
+
+    /**
+     * Called when the application is created
+     */
+    override fun onCreate(){
+        super<Application>.onCreate()
+        // Register the lifecycle observer
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
+        Log.d(tag, "Application created")
+    }
+
+    /**
+     * Called when the application is started
+     */
+    override fun onStart(owner: LifecycleOwner){
+        super.onStart(owner)
+        // Handle offline/off app state gap/ catch up calculation
+        appLifecycleManager.handleAppResumed()
+        Log.d(tag, "Application started")
+    }
+
+    /**
+     * Called when the application is stopped
+     */
+    override fun onStop(owner: LifecycleOwner){
+        super.onStop(owner)
+        // When the app is closed/unfocused, save the timestamp
+        appLifecycleManager.handleAppPaused()
+        Log.d(tag, "Application stopped")
+    }
 }

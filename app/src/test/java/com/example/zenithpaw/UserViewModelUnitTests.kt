ee6795046f10b34b856e05a147a6dd3f25d83d29
Tony Faijue <tonyf17191@gmail.com>
2026-07-23 19:38:05 -0500

    test(UserViewModelUnitTests): user account is created & save profile is clicked

--- app/src/test/java/com/example/zenithpaw/UserViewModelUnitTests.kt
+++ app/src/test/java/com/example/zenithpaw/UserViewModelUnitTests.kt
@@ -9,6 +9,8 @@ import com.example.zenithpaw.roomdatabase.userinventoryitem.UserInventoryItem
 import com.example.zenithpaw.roomdatabase.userinventoryitem.UserInventoryItemRepository
 import com.example.zenithpaw.ui.uievents.UserUiEvent
 import com.example.zenithpaw.ui.viewmodels.UserViewModel
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
@@ -16,6 +18,7 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
@@ -57,11 +60,10 @@ class UserViewModelUnitTests {
         // Make sure the list of users is empty
         every { userRepository.getUsers() } returns flowOf(emptyList())
 
-        // Act
         // Initialize the view model
         val viewModel = UserViewModel(userRepository, userInventoryItemRepository, shopItemRepository, testDispatcher)
 
-        // Assert
+        // Act
         // test{} handles asserting the emissions from the flow
         viewModel.uiState.test {
             // Assert the initial state event
@@ -90,11 +92,10 @@ class UserViewModelUnitTests {
         every { userInventoryItemRepository.getUserInventoryItemsByUserId(testUser.userId) } returns flowOf(emptyList())
         every { shopItemRepository.getShopItems() } returns flowOf(emptyList())
 
-        // Act
         // Initialize the view model
         val viewModel = UserViewModel(userRepository, userInventoryItemRepository, shopItemRepository, testDispatcher)
 
-        // Assert
+        // Act
         // test{} handles asserting the emissions from the flow
         viewModel.uiState.test {
             // Assert the initial state
@@ -133,6 +134,7 @@ class UserViewModelUnitTests {
 
         val viewModel = UserViewModel(userRepository, userInventoryItemRepository, shopItemRepository, testDispatcher)
 
+        // Act
         viewModel.uiState.test {
             val initialState = awaitItem() // Initial loading state
             assert(initialState.isLoading)
@@ -149,10 +151,84 @@ class UserViewModelUnitTests {
             val updatedDbUser = initialUser.copy(gold = 500)
             userFlow.value = listOf(updatedDbUser)
 
-            // Assert -> The name should not have changed AND gold is updated
+            // Assert -> The name should have changed AND gold is updated
             val finalState = awaitItem()
             assertEquals(500, finalState.gold)
             assertEquals("NewName", finalState.name)
+
+            assertEquals(false, finalState.isLoading)
+        }
+    }
+
+    @Test
+    fun `when user account is created, new user is added to the database`() = runTest(testDispatcher){
+        // Arrange
+        every { userRepository.getUsers() } returns flowOf(emptyList())
+
+        coEvery { userRepository.deleteAllUsers() } returns Unit
+        coEvery {userRepository.upsertUser(any())} returns Unit
+
+        val viewModel = UserViewModel(userRepository, userInventoryItemRepository, shopItemRepository, testDispatcher)
+
+        // Act
+        viewModel.uiState.test {
+            val initialState = awaitItem() // Initial loading state
+            assert(initialState.isLoading)
+
+            // Act
+            viewModel.onEvent(UserUiEvent.OnCreateAccountClicked("JohnDoe", "johndoe@example.com"))
+
+            // Assert
+            val finalState = awaitItem()
+            assertEquals(false, finalState.isRegisteringDialogVisible)
+            assertEquals(false, finalState.isLoading)
+
+
+            coVerify(exactly = 1) { userRepository.deleteAllUsers() }
+            coVerify(exactly = 1) { userRepository.upsertUser(any()) }
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `when save profile is clicked, changes are saved to the database`() = runTest(testDispatcher){
+        // Arrange
+        val testUser = User("JohnDoe", "johndoe@example.com", "imageurl.com", 500L, 0, "1")
+        val testUsers = listOf(testUser)
+
+        every { userRepository.getUsers() } returns flowOf(testUsers)
+        every { userInventoryItemRepository.getUserInventoryItemsByUserId(testUser.userId) } returns flowOf(emptyList())
+        every { shopItemRepository.getShopItems() } returns flowOf(emptyList())
+
+        coEvery {userRepository.upsertUser(any())} returns Unit
+
+        val viewModel = UserViewModel(userRepository, userInventoryItemRepository, shopItemRepository, testDispatcher)
+
+        viewModel.uiState.test {
+            val initialState = awaitItem() // Initial loading state
+            assert(initialState.isLoading)
+
+            val loadedState = awaitItem() // Loaded state with initial data
+            assertEquals(testUser.name, loadedState.name)
+
+            // Act
+            viewModel.onEvent(UserUiEvent.OnNameChange("NewName"))
+            val typedStateName = awaitItem()
+            assertEquals("NewName", typedStateName.name)
+
+            viewModel.onEvent(UserUiEvent.OnEmailChange("newemail@example.com"))
+            val typedStateEmail = awaitItem()
+            assertEquals("newemail@example.com", typedStateEmail.email)
+
+            viewModel.onEvent(UserUiEvent.OnSaveProfileClicked)
+            advanceUntilIdle()
+
+            // Assert
+            assertEquals("NewName", typedStateEmail.name)
+            assertEquals("newemail@example.com", typedStateEmail.email)
+            assertEquals(false, typedStateEmail.isLoading)
+
+            coVerify(exactly = 1) { userRepository.upsertUser(any()) }
         }
     }
 }
